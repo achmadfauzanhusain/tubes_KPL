47dc796f91db1c10bcd69607d69faf32b0f23ce2
fauzanchenko <79782055+achmadfauzanhusain@users.noreply.github.com>
2026-06-18 13:20:01 +0700

    Change ValidationHelper to a static class and add ValidationResult

--- tubes_kpl/Helpers/ValidationHelper.cs
+++ tubes_kpl/Helpers/ValidationHelper.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Tubes_KPL.Helpers
 {
-    internal class ValidationHelper
+    public static class ValidationHelper
     {
+    }
+
+    public class ValidationResult
+    {
+        
     }
 }
